437160e30e69c02732e3d08c616db3ac4b32b228
Dabloo Gupta <dabloogupta67@gmail.com>
2023-02-13 19:52:35 +0530

    hello

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+        public int Product(int x, int y) {
+            return x*y;
+        }
+
+        public int Product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double Product(double x, double y) {
+            return x*y;
+        }
+    }
+    public static void main(String[]args){
+        Product p = new Product();
+        p.Product(1,2);
+        p.Product(1,2,3);
+        p.Product(1.2,2.0);
+    }
 }
