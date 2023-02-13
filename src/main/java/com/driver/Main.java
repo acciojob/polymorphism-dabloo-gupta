7da66e6b4b40db56588e0f1fdf31225ba1504151
Dabloo Gupta <dabloogupta67@gmail.com>
2023-02-13 20:10:47 +0530

    hello

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,22 +3,22 @@ package com.driver;
 public class Main {
     public static class Product{
 
-        public int Product(int x, int y) {
+        public int product(int x, int y) {
             return x*y;
         }
 
-        public int Product(int x, int y, int z) {
+        public int product(int x, int y, int z) {
             return x*y*z;
         }
 
-        public double Product(double x, double y) {
+        public double product(double x, double y) {
             return x*y;
         }
     }
     public static void main(String[]args){
-//        Product p = new Product();
-//        p.Product(1,2);
-//        p.Product(1,2,3);
-//        p.Product(1.2,2.0);
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2,2.0);
     }
 }
